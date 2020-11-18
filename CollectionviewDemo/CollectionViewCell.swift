c5ed5221c0760ea195fb69998543b279ba94d339
sireeesha <sireesha@krify>
2020-11-18 09:28:44 +0530

    First Commit

---
+++ CollectionviewDemo/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  CollectionviewDemo
+//
+//  Created by apple on 17/11/20.
+//  Copyright © 2020 Krify. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var label: UILabel!
+    @IBOutlet var imgView: UIImageView!
+}
--- CollectionviewDemo/ViewController.swift
+++ CollectionviewDemo/ViewController.swift
@@ -8,11 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout
+{
+    var arr = ["youtube.jpg","profile.jpg","patientblack.jpg"]
+    var arr1=["India","Germany","England"]
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if section == 0{
+            return arr.count
+        }
+        else if section == 1{
+            return arr.count
+        }
+        else{
+            return arr.count
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell=collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+        cell.imgView.layer.cornerRadius=cell.imgView.frame.height/2
+        if indexPath.section == 0{
+            cell.imgView.image=UIImage(named:arr[indexPath.row])
+            cell.label.text = arr1[indexPath.row]
+        }
+        else if indexPath.section == 1{
+            cell.imgView.image=UIImage(named:arr[indexPath.row])
+            cell.label.text = arr1[indexPath.row]
+        }
+        else {
+            cell.imgView.image=UIImage(named:arr[indexPath.row])
+            cell.label.text = arr1[indexPath.row]
+        }
+        return cell
+    }
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 3
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cellWidth = UIScreen.main.bounds.width-32
+        return CGSize(width: cellWidth/3, height: 156)
+    }
+    @IBOutlet var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //collectionView.collectionViewLayout=self
+        collectionView.delegate=self
+        collectionView.dataSource=self
+        
     }
 
 
